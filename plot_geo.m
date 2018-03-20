c4d8131910a15cd55ee7977f83f890dd3299b582
Gaspar <gavit@pa.uc3m.es>
2018-03-20 12:10:07 +0100

    test

--- plot_geo.m
+++ plot_geo.m
@@ -12,6 +12,7 @@ x_0 = -3.77;
 y_0 = +40.332;
 
 
+
 %% Raw
 % datos = load('/Users/gavit/Documents/Evidance-Based Behavior/Acceso_mongo/test-lg-g4_geowithplaces.mat');
 datos = load('/Users/gavit/Documents/Evidance-Based Behavior/Acceso_mongo/pruebas7.mat');
